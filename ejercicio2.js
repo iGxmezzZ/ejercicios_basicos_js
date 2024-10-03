3399782a4a037b174b711563bfa48746a1115de7
Iván Gómez <ivangu.9@gmail.com>
2024-10-03 15:02:53 +0200

    ej.4

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,7 +1,48 @@
-const jedi = {nombre: "Luke Skywalker", edad: 19};
-
-// Tu código para cambiar la edad aquí...
-
+const jedi = { nombre: "Luke Skywalker", edad: 19 };
 jedi.edad = 25;
 
 console.log(jedi);
+
+//2
+let nombre;
+nombre = "Leia";
+let apellido = "Organa";
+let edad = 20;
+
+console.log(
+  "Soy " +
+    nombre +
+    " " +
+    apellido +
+    ", tengo " +
+    edad +
+    " años y soy una princesa de Alderaan."
+);
+
+//3
+const sable1 = { nombre: "Shoto de Yoda", precio: 1500 };
+const sable2 = { nombre: "Sable de Darth Vader", precio: 2000 };
+
+let precio = sable1.precio + sable2.precio;
+console.log(precio);
+
+//4
+let precioBaseGlobal = 10000;
+// Cambia el precio base en esta linea modificando la variable
+precioBaseGlobal= 25000;
+
+const nave1 = {
+  nombre: "Ala-X",
+  precioBase: 50000,
+  precioFinal: 60000,
+};
+const nave2 = {
+  nombre: "Halcón Milenario",
+  precioBase: 70000,
+  precioFinal: 80000,
+};
+
+// Tu código para actualizar el precio final de cada nave aquí...
+[nave1.precioFinal, nave2.precioFinal] = [nave1.precioBase + precioBaseGlobal, nave2.precioBase + precioBaseGlobal]
+console.log(nave1);
+console.log(nave2);
---
+++ ejercicio3.js
@@ -0,0 +1,23 @@
+let primero = 10 * 5;
+console.log(primero);
+
+let segundo = 10 / 2;
+console.log(segundo);
+
+let tercero = 15 % 9;
+console.log(tercero);
+
+function cuarto() {
+  let p = 10;
+  let j = 5;
+  let o = p + j;
+  return o;
+}
+let resultadoCuarto = cuarto();
+console.log(resultadoCuarto);
+
+let c = 10;
+let m = 5;
+let i = c*m;
+
+console.log(i);
